3f621e40e3495be882286cc830d2131f4edc15dd
Natan Cardoso <natansc73@gmail.com>
2022-11-19 14:05:12 -0300

    Styling product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import shirt01Img from '../assets/shirts/shirt01.svg'
+import shirt02Img from '../assets/shirts/shirt02.svg'
+import shirt03Img from '../assets/shirts/shirt03.svg'
+import shirt04Img from '../assets/shirts/shirt04.svg'
+
 export default function Home() {
   return (
-    <h1>oi</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt01Img} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>
+            Camiseta X
+          </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt02Img} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>
+            Camiseta Y
+          </strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
